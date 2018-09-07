ecacecc51f7fa3fb82ad8858b52f0a22b7626f7d
Joerg Kastning <joerg.kastning@uni-bielefeld.de>
2018-09-07 10:06:12 +0200

    Changed stage labels against phase labels.

--- run_rhel_patch_mgmt.sh
+++ run_rhel_patch_mgmt.sh
@@ -3,11 +3,13 @@
 DOM=`date +%d`
 DOW=`date +%w`
 LOG="/var/log/ansible/patch_run_`date +%Y-%m-%d`.log"
+SETUP_LOG="/var/log/ansible/setup_patch_run_`date +%Y-%m-%d`.log"
 SSH_KEY="PUT PATH TO PRIVATE KEY HERE"
 PLAYBOOK="/path/to/patch_rhel.yml"
+CREATEVARS="/path/to/ansible/roles/rhel-patchmanagement/create_vars.sh"
 
 # Run Patch-Management ad-hoc in the specified stage
-# Example: './run_rhel_patch_mgmt.sh NOW t-stage'
+# Example: './run_rhel_patch_mgmt.sh NOW rhel-patch-phase1'
 if [ "${1}" = "NOW" ] && [ -n "${2}" ]
 then
   ansible-playbook $PLAYBOOK --private-key=$SSH_KEY --limit="${2}" >> $LOG 2>&1
@@ -17,30 +19,36 @@ fi
 if [ "${1}" = "NOW" ] && [ -z "${2}" ]
 then
   echo "ERROR: Second argument is missing."
-  echo "Example: './run_rhel_patch_mgmt.sh NOW t-stage'"
+  echo "Example: './run_rhel_patch_mgmt.sh NOW rhel-patch-phase1'"
   exit
 fi
 
-# Patchcycle of the T-Stage on the second Tuesday of a month
+# Setup the next patchcycle
+if [ "$DOW" = "2" ] && [ "$DOM" -gt 0 ] && [ "$DOM" -lt 8 ]
+then
+    $CREATEVARS > $SETUP_LOG 2>&1
+fi
+
+# Patchcycle of the rhel-patch-phase1 on the second Tuesday of a month
 if [ "$DOW" = "2" ] && [ "$DOM" -gt 7 ] && [ "$DOM" -lt 15 ]
 then
-    ansible-playbook $PLAYBOOK --private-key=$SSH_KEY --limit=t-stage > $LOG 2>&1
+    ansible-playbook $PLAYBOOK --private-key=$SSH_KEY --limit=rhel-patch-phase1 > $LOG 2>&1
 fi
 
-# Patchcycle of the Q-Stage on the third Tuesday of a month
+# Patchcycle of the rhel-patch-phase2 on the third Tuesday of a month
 if [ "$DOW" = "2" ] && [ "$DOM" -gt 14 ] && [ "$DOM" -lt 22 ]
 then
-    ansible-playbook $PLAYBOOK --private-key=$SSH_KEY --limit=q-stage >> $LOG 2>&1
+    ansible-playbook $PLAYBOOK --private-key=$SSH_KEY --limit=rhel-patch-phase2 > $LOG 2>&1
 fi
 
-# Patchcycle of the P-Stage-1 on the fourth Tuesday of a month
+# Patchcycle of the rhel-patch-phase3 on the fourth Tuesday of a month
 if [ "$DOW" = "2" ] && [ "$DOM" -gt 21 ] && [ "$DOM" -lt 29 ]
 then
-    ansible-playbook $PLAYBOOK --private-key=$SSH_KEY --limit=p-stage1 > $LOG 2>&1
+    ansible-playbook $PLAYBOOK --private-key=$SSH_KEY --limit=rhel-patch-phase3 > $LOG 2>&1
 fi
 
-# Patchcycle of the P-Stage-2 on the fourth Wednesday of a month
+# Patchcycle of the rhel-patch-phase4 on the fourth Wednesday of a month
 if [ "$DOW" = "3" ] && [ "$DOM" -gt 21 ] && [ "$DOM" -lt 30 ]
 then
-    ansible-playbook $PLAYBOOK --private-key=$SSH_KEY --limit=p-stage2 > $LOG 2>&1
+    ansible-playbook $PLAYBOOK --private-key=$SSH_KEY --limit=rhel-patch-phase4 > $LOG 2>&1
 fi
